354ef85610cb7724590c955979d318e4755cf70b
PARZIVAL <mendoncaalexandrejoaorafael@gmail.com>
2026-07-07 10:57:35 +0000

    commits

---
+++ BIMESTRE_2/Programação em camadas/Projeto/models/clientedao.py
@@ -0,0 +1,45 @@
+# DAO = DATA ACCESS OBJECT
+from models.cliente import Cliente
+from pathlib import Path
+import json
+
+class ClienteDAO: 
+    def __init__(self): 
+        self.__arquivo = Path(__file__).resolve().parent / "clientes.json"
+        self.__objetos = []
+        self.__abrir()
+    def inserir(self, obj): 
+        self.__objetos.append(obj)
+        self.__salvar()
+    def listar(self): 
+        return self.__objetos
+    def listar_id(self, id): 
+        for obj in self.__objetos: 
+            if obj.get_id() == id: return obj
+        return None
+    def atualizar(self, obj): 
+        aux = self.listar_id(self.get_id)
+        if aux != None: 
+            self.__objetos.remove(aux)
+            self.__objetos.append(obj)
+            self.__salvar()
+    def excluir(self, id): 
+        aux = self.listar_id(id)
+        if aux != None: 
+            self.__objetos.remove(aux)
+            self.salvar()
+    def __abrir(self): 
+        try: 
+            arquivo = open(self.__aquivo, mode = 'r')
+            list_dic = json.load(arquivo)
+            arquivo.close()
+            self.__objetos = []
+            for dic in list_dic: 
+                obj = Cliente.from_json(dic)
+                self.__objetos.append(obj)
+        except FileExistsError: 
+            pass
+    def __salvar(self): 
+        arquivo = open(self.__arquivo, mode = 'w')
+        json.dump(self.__objetos, arquivo, default = Cliente.to_json, indent = 2)
+        arquivo.close()
---
+++ BIMESTRE_2/Programação em camadas/Projeto/ui.py
@@ -0,0 +1,42 @@
+from service import Service
+
+class UI: 
+    @staticmethod
+    def main(): 
+        op = 0
+        while op != 9: 
+            op = UI.menu()
+            match op: 
+                case 1: UI.cliente_inserir()
+                case 2: UI.cliente_listar()
+                case 3: UI.cliente_atualizar()
+                case 4: UI.cliente_excluir()
+    @staticmethod
+    def menu(): 
+        print(' [1] INSERIR \n [2] LISTAR \n [3] ATUALIZAR \n [4] EXCLUIR \n [9] FIM')
+        return int(input('Informe uma opção: '))
+    @staticmethod
+    def cliente_inserir(): 
+        id = int(input('Informe o ID: \n'))
+        nome = input('Informe o nome: \n')
+        email = input('Informe o email: \n')
+        fone = input('Informe o telefone: \n')
+        Service.cliente_inserir(id, nome, email, fone)
+    @staticmethod
+    def cliente_listar(): 
+        for obj in Service.cliente_listar(): print(obj)
+    @staticmethod
+    def cliente_atualizar(): 
+        for obj in Service.cliente_listar(): print(obj)
+        id = int(input('Informe o ID a ser atualizado: \n'))
+        nome = input('Informe o novo nome: \n')
+        email = input('Informe o novo email: \n')
+        fone = input('Informe o novo telefone: \n')
+        Service.cliente_atualizar(id, nome, email, fone)
+    @staticmethod
+    def cliente_excluir():
+        for obj in Service().cliente_listar(): print(obj)
+        id = int(input('Informe o id do cliente a ser excluido: \n'))
+        Service.cliente_excluir(id)
+
+UI.main()
